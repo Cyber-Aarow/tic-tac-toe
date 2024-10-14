8157f3bc4f0800d7a18d0702b4785fdf7ba293d1
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-10-13 22:17:43 -0700

    Make basic JS objects.

---
+++ tictactoe.js
@@ -0,0 +1,20 @@
+const Player = (letter) => {
+    let score = 0;
+    return {
+
+    }
+};
+
+const Game = () => {
+
+};
+
+const GameBoard = () => {
+    let board = [];
+    
+    function newBoard(){
+        for(let i = 0; i < 9; i++) board.push(0);
+    }
+
+
+};
