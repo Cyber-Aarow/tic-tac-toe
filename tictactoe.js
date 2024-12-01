66b08d90242fadbfe5265c1324217b183d2d23c1
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-11-30 18:24:47 -0800

    Fix the 'if' statements for the winCheck methods. It's ok to wish 'a === b === c' worked, but must stop trying it.

--- tictactoe.js
+++ tictactoe.js
@@ -84,68 +84,68 @@ function Game(){
 
         //Rows
         const rowOneCheck = () => {
-            if(board[0] === board[1] === board[2] && board[0] === 1){
+            if((board[0] === board[1]) && (board[0] === board[2]) && (board[0] === 1)){
                 win1 = true;
             }
-            else if(board[0] === board[1] === board[2] && board[0] === 1){
+            else if((board[0] === board[1]) && (board[0] === board[2]) && (board[0] === 1)){
                 win2 = true;
             }
         };
         const rowTwoCheck = () => {
-            if(board[3] === board[4] === board[5] && board[3] === 1){
+            if((board[3] === board[4]) && (board[3] === board[5]) && (board[3] === 1)){
                 win1 = true;
             }
-            else if(board[3] === board[4] === board[5] && board[3] === 1){
+            else if((board[3] === board[4]) && (board[3] === board[5]) && (board[3] === 1)){
                 win2 = true;
             }
         };
         const rowThreeCheck = () => {
-            if(board[6] === board[7] === board[8] && board[6] === 1){
+            if((board[6] === board[7]) && (board[6] === board[8]) && (board[6] === 1)){
                 win1 = true;
             }
-            else if(board[6] === board[7] === board[8] && board[6] === 1){
+            else if((board[6] === board[7]) && (board[6] === board[8]) && (board[6] === 1)){
                 win2 = true;
             }
         };
         //Columns
         const colOneCheck = () => {
-            if(board[0] === board[3] === board[6] && board[0] === 1){
+            if((board[0] === board[3]) && (board[0] === board[6]) && (board[0] === 1)){
                 win1 = true;
             }
-            else if(board[0] === board[3] === board[6] && board[0] === 1){
+            else if((board[0] === board[3]) && (board[0] === board[6]) && (board[0] === 1)){
                 win2 = true;
             }
         };
         const colTwoCheck = () => {
-            if(board[1] === board[4] === board[7] && board[1] === 1){
+            if((board[1] === board[4]) && (board[1] === board[7]) && (board[1] === 1)){
                 win1 = true;
             }
-            else if(board[1] === board[4] === board[7] && board[1] === 1){
+            else if((board[1] === board[4]) && (board[1] === board[7]) && (board[1] === 1)){
                 win2 = true;
             }
         };
         const colThreeCheck = () => {
-            if(board[2] === board[5] === board[8] && board[2] === 1){
+            if((board[2] === board[5]) && (board[2] === board[8]) && (board[2] === 1)){
                 win1 = true;
             }
-            else if(board[2] === board[5] === board[8] && board[2] === 1){
+            else if((board[2] === board[5]) && (board[2] === board[8]) && (board[2] === 1)){
                 win2 = true;
             }
         };
         //Diagonals
         const diagLTRCheck = () => {
-            if(board[0] === board[4] === board[8] && board[0] === 1){
+            if((board[0] === board[4]) && (board[0] === board[8]) && (board[0] === 1)){
                 win1 = true;
             }
-            else if(board[0] === board[4] === board[8] && board[0] === 1){
+            else if((board[0] === board[4]) && (board[0] === board[8]) && (board[0] === 1)){
                 win2 = true;
             }
         };
         const diagRTLCheck = () => {
-            if(board[2] === board[4] === board[6] && board[2] === 1){
+            if((board[2] === board[4]) && (board[2] === board[6]) && (board[2] === 1)){
                 win1 = true;
             }
-            else if(board[2] === board[4] === board[6] && board[2] === 1){
+            else if((board[2] === board[4]) && (board[2] === board[6]) && (board[2] === 1)){
                 win2 = true;
             }
         };
